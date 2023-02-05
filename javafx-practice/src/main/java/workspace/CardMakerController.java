4860c2796f452f4f3d57f11f21e13a0f1e98df81
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-02-05 20:32:46 +0800

    Create TODO for CardMaker Controller
    Listed all the necessary methods for the CardMakerController with a specified TODO to guide efforts

--- javafx-practice/src/main/java/workspace/CardMakerController.java
+++ javafx-practice/src/main/java/workspace/CardMakerController.java
@@ -9,8 +9,40 @@ public class CardMakerController {
 
     @FXML
 
-    public void changeViewToDefaultScreen(ActionEvent e) throws IOException {
+    private void changeViewToDefaultScreen(ActionEvent e) throws IOException {
         App.setRootForScene("welcomeScreen");
     }
+
+    private void saveCard() {
+        //TODO: Check if both Question and Answer meets minimum/maximum content; if true then parse question and answer, then save into database; else show a dialog telling user to check inputs
+    }
+
+    private String parseQuestion() {
+        String cardQuestion = null;
+        //TODO: Insert code for parsing String text from question text area
+        return cardQuestion;
+    }
+
+    private String parseAnswer() {
+        String cardAnswer = null;
+        //TODO: Insert code for parsing String text from answer text area
+        return cardAnswer;
+    }
+
+    private boolean checkIfQuestionAndAnswerMeetsConditions(int minChars, int maxChars) {
+        boolean checkResult = false;
+        //TODO: Insert code for checking if both question and answers meet the following conditions:(1) has content, (2) meets minimum characters, and (3) meets maximum characters
+        return checkResult;
+    }
+
+    private void saveDataToDatabase() {
+        //TODO: Save question and answer to a database
+    }
+
+    private void showDialogForUserReview() {
+        //TODO: Show a dialog scene showing message for user to review content 
+    }
+
+
 }
 
