1dd8bea799df2bb969a7b2dd28a2358093f2664c
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-02-08 18:39:09 +0800

    Continue Work on CardMakerController
    Wrote functions for different tasks needed for the Save Button functionality

--- javafx-practice/src/main/java/workspace/CardMakerController.java
+++ javafx-practice/src/main/java/workspace/CardMakerController.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class CardMakerController {
 
     @FXML
+
+    TextField questionContainer = new TextField();
+    TextArea answerContainer = new TextArea();
+    Button saveButton = new Button();
 
     private void changeViewToDefaultScreen(ActionEvent e) throws IOException {
         App.setRootForScene("welcomeScreen");
@@ -15,23 +22,56 @@ public class CardMakerController {
 
     private void saveCard() {
         //TODO: Check if both Question and Answer meets minimum/maximum content; if true then parse question and answer, then save into database; else show a dialog telling user to check inputs
+        if (checkIfQuestionAndAnswerMeetsConditions() ==  true) {
+            saveDataToDatabase();
+        } else {}
     }
 
     private String parseQuestion() {
-        String cardQuestion = null;
-        //TODO: Insert code for parsing String text from question text area
+        String cardQuestion = questionContainer.getText();
         return cardQuestion;
     }
 
     private String parseAnswer() {
-        String cardAnswer = null;
-        //TODO: Insert code for parsing String text from answer text area
+        String cardAnswer = answerContainer.getText();
         return cardAnswer;
     }
 
-    private boolean checkIfQuestionAndAnswerMeetsConditions(int minChars, int maxChars) {
+    private boolean checkIfQuestionAndAnswerMeetsConditions() {
         boolean checkResult = false;
-        //TODO: Insert code for checking if both question and answers meet the following conditions:(1) has content, (2) meets minimum characters, and (3) meets maximum characters
+        if (checkIfQuestionAndAnswerHasValue() == true) {
+            if (checkIfQuestionAndAnswerMeetsLimit(5, 250) == true) {
+               checkResult = true;
+            } else {
+                checkResult = false;
+            }
+        } else {
+            checkResult = false;
+        }
+        return checkResult;
+    }
+    
+    private boolean checkIfQuestionAndAnswerHasValue() {
+        boolean checkResult = false;
+        if (parseQuestion() == null || parseAnswer() == null) {
+            showDialogForQuestionAndAnswerBeingNull();
+        } else {
+            checkResult = true;
+        }
+        return checkResult;
+    }
+
+    private boolean checkIfQuestionAndAnswerMeetsLimit(int minChars, int maxChars) {
+        int questionTextCount = parseQuestion().length();
+        int answerTextCount = parseAnswer().length();
+        boolean checkResult = false;
+        if (questionTextCount < minChars || questionTextCount > maxChars) {
+            showDialogForQuestionAndAnswerReachingLimit();
+        } else if (answerTextCount < minChars || answerTextCount > maxChars) {
+            showDialogForQuestionAndAnswerReachingLimit();
+        } else {
+            checkResult = true;
+        } 
         return checkResult;
     }
 
@@ -39,7 +79,11 @@ public class CardMakerController {
         //TODO: Save question and answer to a database
     }
 
-    private void showDialogForUserReview() {
+    private void showDialogForQuestionAndAnswerBeingNull() {
+        //TODO: Show a dialog scene showing message for user to review content 
+    }
+
+    private void showDialogForQuestionAndAnswerReachingLimit() {
         //TODO: Show a dialog scene showing message for user to review content 
     }
 
